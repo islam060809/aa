ebf0ee0f8ec5d84f67f6abdc41f0132c95a0b5c2
Islam <gojo060809@gmail.com>
2024-04-22 23:12:57 +0600

    .

--- j.js
+++ j.js
@@ -8,19 +8,29 @@ delAll.onclick = () => {
 }
 
 btn.onclick = () => {
-     console.log(btn.innerText)
-     console.log(input.value)
-    const liTag = document.createElement("li")
-     liTag.innerText = input.value
-    console.log(liTag)
-    list.appendChild(liTag)
-     input.value = ""
-    liTag.onclick= () =>{
-        liTag.style.textDecoration = "line-through"
-        liTag.onclick= () =>{
-            liTag.style.textDecoration = "none"
-        }
+    if (input.value.trim() === "") {
+        return; 
     }
+    console.log(btn.innerText)
+    console.log(input.value)
+   const liTag = document.createElement("li")
+   const funBtn = document.createElement("button")
+    liTag.innerText = input.value
+    funBtn.innerText = " . "
+   console.log(liTag)
+   list.appendChild(liTag)
+   list.appendChild(funBtn)
+    input.value = ""
+   liTag.onclick= () =>{
+       liTag.style.textDecoration = "line-through"
+       liTag.onclick= () =>{
+           liTag.style.textDecoration = "none"
+       }
+   }
+   funBtn.onclick= () => {
+    liTag.style.color = "green"
+    liTag.style.fontWeight = 1000
+   }
 }
 
 // input.onchange = () => {
